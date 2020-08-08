97e9ac33f2ffbcf8886d47259094820ed67076d8
IrynaGil <irinagilenenko@gmail.com>
2020-08-08 10:19:14 +0300

    added processing of searching products

---
+++ src/main/java/base/BasePage.java
@@ -0,0 +1,52 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
+public class BasePage {
+    private final short defaultTimeout = 3;
+
+    public WebDriver driver;
+
+    public BasePage() {
+        driver = BaseTest.getDriver();
+    }
+
+    public void openURL(String URL) {
+        driver.get(URL);
+    }
+
+    public void waitForVisibilityOfElement(WebElement element, int time) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, time);
+            wait.until(visibilityOf(element));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Boolean isElementVisible(WebElement element) {
+        try {
+            waitForVisibilityOfElement(element, defaultTimeout);
+            element.isDisplayed();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void enterInputValue(WebElement element, String string) {
+        element.sendKeys(string);
+    }
+
+    public void clickOnElement(WebElement element) {
+        element.click();
+    }
+
+}
---
+++ src/test/java/page/SearchAndCountTest.java
@@ -0,0 +1,58 @@
+package page;
+
+import base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class SearchAndCountTest extends BaseTest {
+    private SearchAndCount page;
+
+    private static final String searchRequest = "scrapbook album material";
+
+    @Test
+    public void GetSearchResultsAndCountSum() {
+        page = new SearchAndCount(getDriver());
+        page.openURL("https://www.craftdirect.com/");
+
+        WebElement websiteLogo = page.getWebsiteLogo();
+
+        assertTrue(page.isElementVisible(websiteLogo), "Website Home page is not found");
+
+        WebElement searchField = page.getSearchField();
+        assertTrue(page.isElementVisible(searchField), "Search field is not found");
+
+        page.enterInputValue(searchField, searchRequest);
+
+        WebElement submitButton = page.getSubmitButton();
+        assertTrue(page.isElementVisible(submitButton), "Submit button is not found");
+
+        page.clickOnElement(submitButton);
+
+        WebElement resultCounter = page.getResultCounter();
+        assertTrue(page.isElementVisible(resultCounter), "Search results counter is not found");
+
+        System.out.println("По запиту: " + searchRequest);
+        System.out.println(resultCounter.getText() + " items found");
+
+        WebElement olProductList = getDriver().findElement(By.className("product-items"));
+
+        List<WebElement> productList = olProductList.findElements(By.tagName("li"));
+        System.out.println(productList);
+
+        Double lessThan = 10.0; //$
+
+        Double totalAmountLessThan = 0.0;
+
+        for (WebElement product: productList) {
+            Double regularPrice = 0.0;
+
+        }
+
+
+    }
+}
